54229392ff4e0f38f6183ee3c04bfd2d5e5d2bdc
nk_ysg <nk_ysg@163.com>
2022-05-08 23:03:20 +0800

    add FrozenSubtreeSiblingIterator.java

--- memory/src/main/java/com/ysg/FrozenSubtreeSiblingIterator.java
+++ memory/src/main/java/com/ysg/FrozenSubtreeSiblingIterator.java
@@ -2,14 +2,42 @@ package com.ysg;
 
 import java.util.Iterator;
 
-public class FrozenSubtreeSiblingIterator implements Iterator<FrozenSubtreeSiblingIterator> {
-    @Override
-    public boolean hasNext() {
-        return false;
+public class FrozenSubtreeSiblingIterator implements Iterator<NodeIndex> {
+    private long currentNumLeaves;
+    private long remainingNewLeaves;
+
+    public FrozenSubtreeSiblingIterator(long currentNumLeaves, long newNumLeaves) {
+        this.currentNumLeaves = currentNumLeaves;
+        this.remainingNewLeaves = newNumLeaves - currentNumLeaves;
+    }
+
+    private long nextNewLeafBatch() {
+        int zeros = Long.numberOfLeadingZeros(remainingNewLeaves);
+        return 1L << (63 - zeros);
     }
 
     @Override
-    public FrozenSubtreeSiblingIterator next() {
-        return null;
+    public boolean hasNext() {
+        return remainingNewLeaves != 0;
+    }
+
+    @Override
+    public NodeIndex next() {
+        long nextSubtreeLeaves;
+        if (currentNumLeaves > 0) {
+           long rightmostFrozenSubtreeLeaves = 1L << Long.numberOfTrailingZeros(currentNumLeaves);
+           if (remainingNewLeaves >= rightmostFrozenSubtreeLeaves) {
+               nextSubtreeLeaves = rightmostFrozenSubtreeLeaves;
+           } else {
+               nextSubtreeLeaves = nextNewLeafBatch();
+           }
+        } else {
+            nextSubtreeLeaves = nextNewLeafBatch();
+        }
+        long firstLeafIndex = currentNumLeaves;
+        long lastLeafIndex = firstLeafIndex + nextSubtreeLeaves - 1;
+        currentNumLeaves += nextSubtreeLeaves;
+        remainingNewLeaves -= nextSubtreeLeaves;
+        return NodeIndex.fromInorderIndex(firstLeafIndex + lastLeafIndex);
     }
 }
