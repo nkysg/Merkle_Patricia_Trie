597b8fbc003ee0f8350f4810af83c64d2de85785
nk_ysg <nk_ysg@163.com>
2022-05-08 23:01:54 +0800

    add AncestorSiblingIterator.java

--- memory/src/main/java/com/ysg/AncestorSiblingIterator.java
+++ memory/src/main/java/com/ysg/AncestorSiblingIterator.java
@@ -2,14 +2,22 @@ package com.ysg;
 
 import java.util.Iterator;
 
-public class AncestorSiblingIterator implements Iterator<AncestorSiblingIterator> {
-    @Override
-    public boolean hasNext() {
-        return false;
+public class AncestorSiblingIterator implements Iterator<NodeIndex> {
+    private NodeIndex nodeIndex;
+
+    public AncestorSiblingIterator(NodeIndex nodeIndex) {
+        this.nodeIndex = nodeIndex;
     }
 
     @Override
-    public AncestorSiblingIterator next() {
-        return null;
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public NodeIndex next() {
+        NodeIndex currentSiblingIndex = nodeIndex.sibling();
+        nodeIndex = nodeIndex.parent();
+        return currentSiblingIndex;
     }
 }
